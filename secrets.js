841eeba3436fbbd613738a6418a6bc0cedf48451
ruizry <ruizry@oregonstate.edu>
2018-04-18 23:18:50 -0700

    Delete secrets.js

--- secrets.js
+++
@@ -1,9 +0,0 @@
-const secrets = {
-    'db_uri': 'mongodb://rysdev:rareware@ds247619.mlab.com:47619/rysdev',
-};
-  
-module.exports = {
-    requestSecret: function(s) {
-      return secrets[s];
-    },
-};
