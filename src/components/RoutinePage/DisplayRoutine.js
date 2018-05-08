539f8f02486fbdbf3733ffa9df04dad289c03079
RyanR <ruizry@oregonstate.edu>
2018-05-07 17:13:44 -0700

    Added detail images to routineDisplay

--- src/components/RoutinePage/DisplayRoutine.js
+++ src/components/RoutinePage/DisplayRoutine.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 //import style or import ui library like material ui
+//maybe add lightbox library for zooming in on images
 
 class DisplayRoutine extends Component {
   constructor(props) {
@@ -11,7 +12,10 @@ class DisplayRoutine extends Component {
   render() {
     return (
       <div>
-        <img src={this.props.frontUrl} alt="Routine Image" width="300" height="400"/>
+        <span> 
+          <img src={this.props.frontUrl} alt="Routine Image" width="300" height="400"/>
+          <img src={this.props.backUrl} alt="Routine Details" width="300" height="400"/>
+        </span>
         <p> { this.props.sets } Sets of { this.props.reps } </p>
       </div>
     )
